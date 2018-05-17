3a64c9e92dfed8b6fcf9ff205284ef40fabeb434
xiaodong-zero <18280054547@163.com>
2018-05-17 17:55:55 +0800

    change

--- website/blog/src/App.js
+++ website/blog/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-
+import { Grid, withStyles, Paper, Typography } from 'material-ui';
+const styles = theme => ({
+  root: {
+    marginTop: 50,
+  },
+  list: {
+    border: "solid 1px rgba(160, 160, 160, 0.3)"
+  }
+})
 class App extends Component {
   render() {
+
+    const { classes } = this.props
     return (
       <div className="App">
         <Header/>
+        <Grid container justify="center" className={classes.root}>
+        <Grid></Grid>
+          <Grid item xs={12} sm={12} md={3} elevation={1}>
+            sldjfldfjlsd
+          </Grid>
+          <Grid className={classes.list} item xs={12} sm={12} md={7}>
+            dsfsdfsdf
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- website/blog/src/components/Header.js
+++ website/blog/src/components/Header.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, withStyles, List, ListItem, Drawer } from "material-ui";
+import { AppBar, Toolbar, IconButton, Typography, withStyles, List, ListItem, Drawer, Button, TextField, Grid, InputAdornment, withWidth, Hidden } from "material-ui";
 import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import compose from "recompose/compose"
 
-const styles = {
+const styles = theme => ({
     root: {
         flexGrow: 1,
     },
@@ -12,45 +14,66 @@ const styles = {
     menuButton: {
         marginLeft: -12,
         marginRight: 20
+    },
+    margin: {
+        margin: theme.spacing.unit
     }
-}
+})
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {drawer:false}
+        this.state = { drawer: false }
         this.toggleDrawer = this.toggleDrawer.bind(this);
     }
 
-    toggleDrawer(){
-        this.setState = {
-            drawer: !this.state.drawer
-        }
+    toggleDrawer = (open) => () => {
+        console.log(this.state)
+        this.setState({
+            drawer: open
+        })
     }
 
     render() {
         const { classes } = this.props;
         const fullList = (
             <List>
-                <ListItem>首页</ListItem>
+                <ListItem><Button>首页</Button></ListItem>
+                <ListItem><Button>分享</Button></ListItem>
+                <ListItem><Button>笔记</Button></ListItem>
+                <ListItem><Button>个人简历</Button></ListItem>
             </List>
         )
         return (
             <div className={classes.root}>
-                <AppBar position="static" >
+                <AppBar color="inherit" position="static" >
                     <Toolbar>
                         <Typography variant="title" align="left" color="inherit" className={classes.flex}>
                             Title
                         </Typography>
-                        <IconButton className={classes.menuButton} onClick={this.toggleDrawer} color="inherit" aria-label="Menu">
+                        <Hidden smDown>
+                        <Grid>
+                            <TextField
+                                className={classes.margin}
+                                id="input-with-icon-textfield"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <SearchIcon />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Grid>
+                        </Hidden>
+                        <IconButton onClick={this.toggleDrawer(true)} color="inherit" aria-label="Menu">
                             <MenuIcon />
                         </IconButton>
-                        <Drawer anchor="right" open={this.state.drawer} onClose={this.toggleDrawer}>
+                        <Drawer anchor="right" open={this.state.drawer} onClose={this.toggleDrawer(false)}>
                             <div
                                 tabIndex={0}
-                                role="right"
-                                onClick={this.toggleDrawer}
-                                onKeyDown={this.toggleDrawer}
+                                onClick={this.toggleDrawer(false)}
+                                onKeyDown={this.toggleDrawer(false)}
                             >
                                 {fullList}
                             </div>
@@ -62,4 +85,4 @@ class Header extends Component {
     }
 }
 
-export default withStyles(styles)(Header);
+export default compose(withStyles(styles), withWidth())(Header);
